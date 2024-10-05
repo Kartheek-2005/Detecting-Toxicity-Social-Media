e4479574a5fc19d85600e8ad3df9a535cd184c80
Naman Chhibbar <n.chhibar@gmail.com>
2024-10-05 10:12:29 +0530

    Fixed issues

--- pinecone_utils.py
+++ pinecone_utils.py
@@ -4,10 +4,10 @@ from pinecone import Pinecone
 class Pinecone_DB:
   """
   Class to interact with Pinecone API.
-  
-  ## Parameters
-    `api_key`: Pinecone API key.
-    `index_name`: Name of the index to connect to.
+
+  Parameters:
+    api_key (str): Pinecone API key.
+    index_name (str): Name of the index to connect to.
   """
 
   def __init__(
@@ -24,34 +24,38 @@ class Pinecone_DB:
     # Connect to index
     self.index = self.pc.Index(index_name)
 
-    self.num_vectors = self.index.describe_index_stats().total_vector_count
+    # Get number of vectors in each namespace
+    self.num_vectors = {
+      namespace: info.vector_count
+      for namespace, info in self.index.describe_index_stats().namespaces.items()
+    }
   
   def embed(
     self,
-    texts: list[str]
-  ) -> list[dict[str, any]]:
+    texts: list[str],
+    start_id: int = 0
+  ) -> list[dict[str, str | list[float] | dict[str, str]]]:
     """
     Embed a list of texts using a Pinecone inference model.
     
-    ## Parameters
-      `texts`: List of texts to embed.
+    Parameters:
+      texts (list[str]): List of texts to embed.
+      start_id (int, default = 0): Starting ID for the embeddings.
 
-    ## Returns
-      `embeddings`: List of dicts to upsert in Pinecone index.
+    Returns:
+      embeddings (list[dict]): List of dicts to upsert in Pinecone index.
     """
 
     # API call to Pinecone
     response = self.pc.inference.embed(
       "multilingual-e5-large",
       inputs = texts,
-      parameters = {
-        "input_type": "passage"
-      }
+      parameters = {"input_type": "passage"}
     )
 
     # Extract embeddings from response
     embeddings = [{
-      "id": str(self.num_vectors + i),
+      "id": str(start_id + i),
       "values": embedding.values,
       "metadata": {"text": text}
     } for i, (text, embedding) in enumerate(zip(texts, response))
@@ -67,18 +71,22 @@ class Pinecone_DB:
     """
     Insert a list of texts into the Pinecone index.
     
-    ## Parameters
-      `texts`: List of texts to embed.
+    Parameters:
+      texts (list[str]): List of texts to embed.
+      namespace (str, default = ""): Namespace to insert the vectors.
     """
 
     # Embed the texts
     embeddings = self.embed(texts)
 
+    # Get start id
+    start_id = self.num_vectors.get(namespace, 0)
+
     # Insert embeddings into Pinecone index
     self.index.upsert(embeddings, namespace)
 
-    # Update id
-    self.num_vectors += len(embeddings)
+    # Update number of vectors in the namespace
+    self.num_vectors[namespace] = start_id + len(embeddings)
 
   def query(
     self,
@@ -89,11 +97,12 @@ class Pinecone_DB:
     """
     Query the Pinecone index and get the top matches.
 
-    ## Parameters
-      `text`: Query text.
-      `n`: Number of matches to return.
+    Parameters:
+      text (str): Query text.
+      n (int, default = 3): Number of matches to return.
+      namespace (str, default = ""): Namespace to query.
     
-    ## Returns
+    Returns:
       `matches`: List of matching texts.
     """
 
@@ -120,7 +129,10 @@ class Pinecone_DB:
   ) -> None:
     """
     Delete all vectors from the Pinecone index.
+
+    Parameters:
+      namespace (str, default = ""): Namespace to delete vectors from.
     """
 
     self.index.delete(delete_all=True, namespace=namespace)
-    self.num_vectors = 0
+    self.num_vectors[namespace] = 0
