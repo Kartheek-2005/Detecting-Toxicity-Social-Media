b82ce3695fc618f3f4be1e0bab6d67deb82a7e60
Naman Chhibbar <n.chhibar@gmail.com>
2024-10-24 15:56:38 +0530

    Replaced .env file with configs.py; added empty notebook train.py

---
+++ configs_example.py
@@ -0,0 +1,14 @@
+"""
+Contains configurations for the project.
+
+This is a template for `configs.py`.
+Fill in the values and save the file as `configs.py`.
+"""
+
+# Pinecone API key and index name for connection
+PINECONE_API_KEY = ""
+PINECONE_INDEX = ""
+
+# Namespaces for toxic and benign examples
+TOXIC_NAMESPACE = ""
+BENIGN_NAMESPACE = ""
